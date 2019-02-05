94dac26e52530f011bf207a910b83964228a0691
Arlette Slachmuylder <arlette.slachmuylder@pcc.edu>
2019-02-04 22:15:34 -0800

    added main method that prints hello-world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,9 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Arlette Slachmuylder on 02/04/2019
+ *
+ */
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
